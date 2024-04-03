f7ae06e72e714dd3fcd60e7f658692a6b4f99a64
Bruno <brunosandrade21@gmail.com>
2024-04-02 21:24:19 -0300

    wpf app client initial commit

---
+++ Client/Views/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace Client;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void LoginButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Login ok
+        LoginGrid.Visibility = Visibility.Collapsed;
+        MainGrid.Visibility = Visibility.Visible;
+    }
+
+    private void LogoutButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Logout ok
+        MainGrid.Visibility = Visibility.Collapsed;
+        LoginGrid.Visibility = Visibility.Visible;
+    }
+}
